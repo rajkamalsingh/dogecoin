fef417e9ff63a1db99ccc05ce4604f1ffc544dcb
rajkamalsingh <36530669+rajkamalsingh@users.noreply.github.com>
2022-05-19 23:49:59 +0530

    Create train_predict.py

---
+++ train_predict.py
@@ -0,0 +1,8 @@
+from autots import AutoTS
+model = AutoTS(forecast_length=10, frequency='infer', ensemble='simple', drop_data_older_than_periods=200)
+model = model.fit(data, date_col='Date', value_col='Close', id_col=None)
+
+prediction = model.predict()
+forecast = prediction.forecast
+print("DogeCoin Price Prediction")
+print(forecast)
